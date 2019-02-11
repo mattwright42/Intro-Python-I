949d2ebfc75bf90140e83d79c8b24a266108169a
Matthew Wright <matthewbwright@gmail.com>
2019-02-11 16:40:38 -0700

    completes args.py

--- src/args.py
+++ src/args.py
@@ -6,6 +6,11 @@
 
 # YOUR CODE HERE
 
+
+def f1(x, y):
+    return(x + y)
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
@@ -13,15 +18,21 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+
+def f2(*x):
+    return(sum(x))
+
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
+print(f2(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20))
 
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -29,12 +40,17 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+
+def f3(x, y=1):
+    return(x + y)
+
+
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
-# prints ouf the keys and values like so:
+# prints out the keys and values like so:
 #
 # key: foo, value: bar
 # key: baz, value: 12
@@ -42,11 +58,17 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
 
-# Should print
-# key: a, value: 12
-# key: b, value: 30
+    # Should print
+    # key: a, value: 12
+    # key: b, value: 30
+    for key, value in kwargs.items():
+        print(f"key: {key}, value: {value}")
+
+
 f4(a=12, b=30)
+
 
 # Should print
 # key: city, value: Berkeley
@@ -60,4 +82,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
